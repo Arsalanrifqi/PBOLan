40f064ab5a9ec765d2734c622634bdb38ad9af61
Arsalanrifqi <62550770+Arsalanrifqi@users.noreply.github.com>
2020-04-01 18:19:51 +0700

    add

--- thread1/ThreadInterface.java
+++ thread1/ThreadInterface.java
@@ -1,15 +1,13 @@
-class mainThread
-{
-public void jalan(String name)
-{
-for(int i=0;i<5;i++)
-{
-try {
-Thread.sleep(1000);
-} catch (InterruptedException e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
-}
+class mainThread{
+public void jalan(String name){
+	for(int i=0;i<5;i++)
+	{
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 System.out.println("Thread: "+name+" posisi: "+i);
 }
 }
