9e1981ee9fd5a54b80b8cc5df38e21e92dc2abbb
Arsalanrifqi <125-arsalan_rifqi@users.noreply.gitd3ti.mipa.uns.ac.id>
2020-04-08 15:31:10 +0700

    merapikan

--- PboLan/PBOLanDaring/src/awt2/HasilAWT.java
+++ PboLan/PBOLanDaring/src/awt2/HasilAWT.java
@@ -1,10 +1,10 @@
-package AWT;
+package awt2;
 
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-public class TampilanAWT extends Frame implements WindowListener{
+public class HasilAWT extends Frame implements WindowListener{
 	String nim;
 	String nama;
 	String jk;
@@ -15,7 +15,7 @@ public class TampilanAWT extends Frame implements WindowListener{
 	Choice cjk1;
 	TextArea calamat;
 	Button b;
-public TampilanAWT(){
+public HasilAWT(){
 	super("Data Diri");
 	Panel p = new Panel();
 	add(p);
@@ -51,18 +51,42 @@ public boolean action(Event e, Object what){
 		alamat = calamat.getText();
 		tmp_lahir = ctmp_lahir.getText();
 		tgl_lahir = ctgl_lahir.getText();
+		new HasilAWT(nim, nama, jk, alamat, tmp_lahir, tgl_lahir);
 		System.out.println(nim);
 		System.out.println(nama);
 		System.out.println(jk);
 		System.out.println(alamat);
 		System.out.println(tmp_lahir);
 		System.out.println(tgl_lahir);
-		}
-	return true;
+		return true;
+	}
+return false;
 }
 
+public HasilAWT(String nim,String nama,String jk,String alamat,String tmp_lahir,
+		String tgl_lahir){
+	super("Identitas Diri");
+	Panel p = new Panel();
+	add(p);
+	p.setLayout(new GridLayout(7, 2));
+	p.add( new Label("nim"));
+	p.add(new Label(nim));
+	p.add( new Label("nama"));
+	p.add(new Label(nama));
+	p.add( new Label("Jenis Kelamin"));
+	p.add(new Label(jk));
+	p.add( new Label("Tempat Lahir"));
+	p.add(new Label(tmp_lahir));
+	p.add( new Label("Tanggal Lahir"));
+	p.add(new Label(tgl_lahir));
+	p.add( new Label("Alamat"));
+	p.add(new Label(alamat));
+	setSize(250,300);
+	setVisible(true);
+	addWindowListener(this);
+}
 public static void main(String args[]){
-	new TampilanAWT();
+	new HasilAWT();
 }
 
 @Override
