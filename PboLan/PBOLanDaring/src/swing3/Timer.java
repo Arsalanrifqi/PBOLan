805f8f3a3fa5f950e7cd07549fe41cee18702360
Arsalanrifqi <125-arsalan_rifqi@users.noreply.gitd3ti.mipa.uns.ac.id>
2020-04-24 10:16:10 +0700

    Update Timer.java

--- PboLan/PBOLanDaring/src/swing3/Timer.java
+++ PboLan/PBOLanDaring/src/swing3/Timer.java
@@ -1,12 +1,17 @@
 package swing3;
 
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -14,19 +19,29 @@ public class Timer extends JFrame implements ActionListener,ChangeListener{
 	JMenuBar mb;
 	JMenu  m;
 	JButton b;
+	JTextField cwaktu;
+	JLabel angka;
+	int waktu;
+	String isi="Start";
+	Frame cp;
 	
 	public Timer(){
-		JFrame cp = new JFrame();
-		cp.setTitle("struk belanja");
-		cp.setSize(500, 400);
-		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame cp = new Frame();
+		cp.setTitle("Timer");
+		cp.setLayout(new FlowLayout());
+		cp.setLayout(new GridLayout(7,2));
 		mb = new JMenuBar();
 		mb.add(m=new JMenu("exit"));
 		m.addChangeListener((ChangeListener) this);;
 		this.setJMenuBar(mb);
-		
+		cp.add(cwaktu = new JTextField());
 		cp.add(b = new JButton("Simpan"));
+		JLabel angka = new JLabel(isi);
+		cp.add(angka);
 		this.setVisible(true);
+		setSize(500,300);
+		setVisible(true);
 		b.addActionListener((ActionListener) this);
 	}
 	
@@ -47,7 +62,12 @@ public class Timer extends JFrame implements ActionListener,ChangeListener{
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		if(arg0.getSource() == b){
-			
+			waktu = Integer.parseInt(cwaktu.getText());
+			while(waktu>0) {
+				isi=Integer.toString(waktu);
+				JLabel angka = new JLabel(isi);
+				cp.delete(angka);
+			}
 		}
 	}
 
